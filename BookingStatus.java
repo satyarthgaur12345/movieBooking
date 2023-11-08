246ec60456d1fff01db11cf4b277914d0dfc1a79
Satyarth Gaur <62906584+satyarthgaur12345@users.noreply.github.com>
2023-11-08 16:36:23 +0530

    Add files via upload

---
+++ BookingStatus.java
@@ -0,0 +1,8 @@
+package com.sample.movielld.enums;
+
+public enum BookingStatus {
+	CREATED,
+	PENDING_FOR_PAYMENT,
+	PAYMENT_FAILED,
+	COMPLETED
+}
---
+++ SeatStatus.java
@@ -0,0 +1,6 @@
+package com.sample.movielld.enums;
+
+public enum SeatStatus {
+	VACANT,
+	BOOKED
+}
